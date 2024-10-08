8607ffb1c57b95d098b4559ef1d58209d29befbc
DESKTOP-ANDVTJN\Administrator <popivnenkomd@gmail.com>
2024-10-08 09:44:21 +0500

    1. Изменил название в хэдере/футере 2. Добавил класс Category с полями: Id, Name, DisplayOrder 3. Добавил Connection String в applicationsettings.json 4. Добавил NuGet пакеты:  EntityFrameworkCore для работы с ORM в проекте  EntityFrameworkCore.SqlServer для подключения к базе данных SQL Server  EntityFrameworkCore.Tools для поддержки миграций и инструментов разработки

--- OnlineBookWeb/obj/Debug/net8.0/OnlineBookWeb.AssemblyInfo.cs
+++ OnlineBookWeb/obj/Debug/net8.0/OnlineBookWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb43a2b8997f1fb88ef733a2bd9a8b85918a114")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abec70db8419a07477687a7135620d2d639c7da2")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

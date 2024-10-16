b0cb45631670492d4a93c0ec68fb07bebf370da2
DESKTOP-ANDVTJN\Administrator <popivnenkomd@gmail.com>
2024-10-16 11:06:09 +0500

    16.10
    1. Создана папка `Data` с классом `ApplicationDbContext`, унаследованным от `DbContext`, для работы с базой данных.
    2. В файле `Program.cs` добавлена строка подключения для `DbContext`, настроена работа с базой данных.
    3. Создана база данных `OnlineBooks` с таблицей `Categories`.
    4. Создан контроллер `CategoryController` для работы с категориями и представление (Index) для отображения данных.
    5. Таблица `Categories` заполнена данными.
    6. Частично настроен вывод данных: контроллер получает данные из базы, но представление пока не настроено для отображения данных на фронте.

--- OnlineBookWeb/Models/Category.cs
+++ OnlineBookWeb/Models/Category.cs
@@ -8,6 +8,6 @@ namespace OnlineBookWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        public string DisplayOrder { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }

b92d236492c2b091e9f84df6b3f5207285c29dc2
DESKTOP-ANDVTJN\Administrator <popivnenkomd@gmail.com>
2024-12-12 16:44:36 +0500

    12.12 1. Добавил DataTables API и загрузил DataTables для таблицы Products 2. Удалил Delete view из папки Product 3. Обновил метод Delete в ProductController.cs 4. Добавил Sweetalert для кнопки удаления 5. Вывел Продукты в виде отдельных карточек на страницу Home с возможностью открытия на отдельной вкладке Details 6. Добавил идентификацию с помощью Scaffolded Items > Identity 7. Добавил авторизацию/логирование на сайте соответственно по кнопкам REGISTER/LOGIN 8. Добавил миграцию addIdentityTables 9. Обновил базу данных, добавив базовые таблицы идентификации: AspNetUserTokens AspNetUsers AspNetUserRoles AspNetUserLogins AspNetUserClaims AspNetRoles AspNetRoleClaims 10. Добавил дополнительные поля в таблицу AspNetUsers: Name StreetAddress City State PostalCode

--- OnlineBook.DataAccess/Data/ApplicationDbContext.cs
+++ OnlineBook.DataAccess/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace OnlineBook.DataAccess.Data
 		}
 		public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
             base.OnModelCreating(modelBuilder);
--- OnlineBook.DataAccess/Migrations/20241212055438_addIdentityTables.Designer.cs
+++ OnlineBook.DataAccess/Migrations/20241212055438_addIdentityTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineBook.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using OnlineBook.DataAccess.Data;
 namespace OnlineBook.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241212055438_addIdentityTables")]
+    partial class addIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -86,11 +89,6 @@ namespace OnlineBook.DataAccess.Migrations
                         .IsConcurrencyToken()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Discriminator")
-                        .IsRequired()
-                        .HasMaxLength(21)
-                        .HasColumnType("nvarchar(21)");
-
                     b.Property<string>("Email")
                         .HasMaxLength(256)
                         .HasColumnType("nvarchar(256)");
@@ -142,10 +140,6 @@ namespace OnlineBook.DataAccess.Migrations
                         .HasFilter("[NormalizedUserName] IS NOT NULL");
 
                     b.ToTable("AspNetUsers", (string)null);
-
-                    b.HasDiscriminator().HasValue("IdentityUser");
-
-                    b.UseTphMappingStrategy();
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserClaim<string>", b =>
@@ -408,28 +402,6 @@ namespace OnlineBook.DataAccess.Migrations
                             Price50 = 22.0,
                             Title = "Leaves and Wonders"
                         });
-                });
-
-            modelBuilder.Entity("OnlineBook.Models.ApplicationUser", b =>
-                {
-                    b.HasBaseType("Microsoft.AspNetCore.Identity.IdentityUser");
-
-                    b.Property<string>("City")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<int>("Name")
-                        .HasColumnType("int");
-
-                    b.Property<string>("PostalCode")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("State")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("StreetAddress")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasDiscriminator().HasValue("ApplicationUser");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
